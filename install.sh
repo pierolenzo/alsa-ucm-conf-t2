e649dc6f9e0a4d9f868e3e2c73988ee2e3a8bf8f
Piero Lenzo <pierolenzo@gmail.com>
2026-04-28 23:21:20 +0200

    add final conf, new install.sh
    Signed-off-by: Piero Lenzo <pierolenzo@gmail.com>

--- install.sh
+++ install.sh
@@ -1,58 +1,48 @@
 #!/usr/bin/env sh
 
-# This script must be run as a normal user as it needs to read the XDG_CONFIG_HOME variable.
-# The user will be asked for their password after saving the variable to a file in /tmp.
-
-if [ $USER != root ]
+# This script must be run as root to install system-wide configuration files.
+if [ "$USER" != "root" ]
 then
-    echo This script must be run as root, you will be prompted for your password
-    echo ${XDG_CONFIG_HOME:-~/.config} | sudo tee /tmp/user.txt >/dev/null
-    sudo chmod 755 $0
-    sudo ./$0
-    sudo rm /tmp/user.txt
+    echo "This script must be run as root. You will be prompted for your password."
+    sudo chmod 755 "$0"
+    sudo "./$0"
     exit 0
 fi
 
-continue=unset
-
-for file in "/usr/share/pulseaudio/alsa-mixer/profile-sets/apple-t2.conf" \
-            "/usr/share/alsa-card-profile/mixer/profile-sets/apple-t2.conf" \
-            "/usr/lib/udev/rules.d/91-pulseaudio-custom.rules" \
-            "/usr/share/alsa/cards/AppleT2.conf"
+echo "Cleaning up any legacy ALSA/PulseAudio configuration files..."
+# Silently remove legacy configuration files (from main branch and older versions)
+for dir in "/usr/share/pulseaudio/alsa-mixer" "/usr/share/alsa-card-profile/mixer"
 do
-    if [ -f "$file" ]
-    then
-        if [ "$continue" = "unset" ]
-        then
-            read -p "Old configuration files are present, do you want to delete them? (y/n) " continue
-
-            if [ "$continue" != "y" ]
-            then
-                break
-            fi
-        fi
-
-        rm -v $file
+    if [ -d "$dir" ]; then
+        rm -f "$dir/profile-sets/apple-t2"*
+        rm -f "$dir/paths/t2-"*
     fi
 done
-
-config_home=$(cat /tmp/user.txt)
-
-for file in "$config_home/pulse/default.pa" \
-            "$config_home/pulse/daemon.conf"
-do
-    if [ -f "$file" ]
-    then
-        echo "You seem to have pulseaudio configuration files present, don't forget to update them if they contain anything specific to T2 Macs"
-        break
-    fi
-done
+rm -f "/usr/lib/udev/rules.d/91-pulseaudio-custom.rules"
+rm -f "/usr/lib/udev/rules.d/91-audio-custom.rules"
+rm -f "/usr/share/alsa/cards/AppleT2.conf"
 
 ucm_dir="/usr/share/alsa/ucm2"
 if [ -d "$ucm_dir" ]
 then
+    echo "Installing ALSA UCM2 profiles..."
     cp -av ucm2/* "$ucm_dir/"
+    
+    echo ""
+    echo "======================================================="
+    echo -e "\033[1;32mInstallation Successful!\033[0m"
+    echo "======================================================="
+    echo "To apply the new audio routing, restart your audio server"
+    echo "by running this command (WITHOUT sudo):"
+    echo ""
+    echo "    systemctl --user restart wireplumber pipewire"
+    echo ""
+    echo -e "\033[1;33mIMPORTANT NOTE FOR FIRST TIME USE:\033[0m"
+    echo "If automatic headphone switching does not work initially,"
+    echo "plug in your headphones and manually select them as the"
+    echo "output device in your desktop audio settings or pwvucontrol."
+    echo "WirePlumber will remember this preference for future automatic switching."
+    echo "======================================================="
 else
-    echo "Directory $ucm_dir not found, UCM2 might not be installed."
+    echo "Error: Directory $ucm_dir not found. ALSA UCM2 might not be installed."
 fi
-
